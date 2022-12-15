9671961550e96ce0328fef73dedd662ede08b19a
Hugo Ramon <ramone.techie@gmail.com>
2022-12-14 23:31:14 -0300

    Added Chakra UI

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../chakra/theme';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />;
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ChakraProvider>
     </SessionProvider>
   );
 }
